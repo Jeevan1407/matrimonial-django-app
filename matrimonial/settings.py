5d1f51de30fafcf1f6795538302d8c7a8c84534f
Sumedh Waghmare <sumedhw71@gmail.com>
2022-12-26 21:38:32 +0530

    rename matromnial to matrimonial

--- matrimonial/settings.py
+++ matrimonial/settings.py
@@ -119,6 +119,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    'matrimonial/static'
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- matrimonial/wsgi.py
+++ matrimonial/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matromonial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matrimonial.settings')
 
 application = get_wsgi_application()
