8a5b434f7dd94fc3e9136679957fbff96179ab95
Sumedh Waghmare <sumedhw71@gmail.com>
2022-12-30 00:01:41 +0530

    signals added

--- core_apps/common/models.py
+++ core_apps/common/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 # Create your models here.
 
+class NonDeleted(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted =False)
+
+
 
 class TimeStampedUUIDModel(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
@@ -14,3 +19,22 @@ class TimeStampedUUIDModel(models.Model):
     class Meta:
         abstract = True
         ordering = ["-created_at", "-updated_at"]
+
+class SoftDelete(models.Model):
+    is_deleted = models.BooleanField(default=False)
+
+    everything = models.Manager()
+    object = NonDeleted
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    manager = True
+
+    class Meta:
+        abstract = True 
